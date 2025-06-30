c4d207f3b3e6138e83f541de058630337887a072
RamonRemo <RamonRemo@users.noreply.github.com>
2025-06-30 22:57:07 +0000

    deploy: a0890342581a546d240a9a73f351970bda8757f1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "590962677"
+    serviceWorkerVersion: "1156664950"
   }
 });
